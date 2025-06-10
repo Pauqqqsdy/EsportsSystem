90824f9e8dcd1a6a0eead1e2a5039eae471e83e7
Paul <pausdyq@gmail.com>
2025-06-11 02:00:42 +0500

    Urls were remade

--- EsportsSystem/urls.py
+++ EsportsSystem/urls.py
@@ -70,9 +70,6 @@ urlpatterns = [
         template_name='registration/password_reset_done.html'
     ), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', views.CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
-        template_name='registration/password_reset_completed.html'
-    ), name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
